d4bd3443b144a1abbf106d3ee75b1b0f704f6e0a
Marco Rebsamen <mrebsamen@unimatrix0.ch>
2021-04-01 22:49:28 +0200

    POC: Interval Messung

--- POC/Form1.cs
+++ POC/Form1.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
@@ -13,15 +15,31 @@ namespace SetBrightness
 {
     public partial class Form1 : Form
     {
+        private YSensor.TimedReportCallback timedReport = new YSensor.TimedReportCallback((sensor, measure) =>
+        {
+            var val = measure.get_averageValue();
+            Debug.Print(val.ToString(CultureInfo.CurrentCulture));
+        });
+
+        private string _err;
+        private Timer _t = new Timer();
         public Form1()
         {
             InitializeComponent();
-            var err = "";
-            YAPI.RegisterHub("USB", ref err);
-            YAPI.UpdateDeviceList(ref err);
+            _t.Interval = 1000;
+            _t.Enabled = true;
+            _t.Tick += (sender, args) =>
+            {
+                YAPI.HandleEvents(ref _err);
+            };
+
+            YAPI.RegisterHub("USB", ref _err);
+            YAPI.UpdateDeviceList(ref _err);
             var sensor = YSensor.FirstSensor();
-            
-            tbLux.Text = sensor.get_advertisedValue();
+            sensor.registerTimedReportCallback(timedReport);
+            _t.Start();
+
+            tbLux.Text = sensor.FriendlyName;
         }
 
         private void btnSet_Click(object sender, EventArgs e)
