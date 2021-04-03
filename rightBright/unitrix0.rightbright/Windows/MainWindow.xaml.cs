877e12f8ab16fa6368fc1585349d0707edb5b9b6
Marco Rebsamen <mrebsamen@unimatrix0.ch>
2021-04-03 21:42:40 +0200

    Beginning GUI Design

---
+++ rightBright/unitrix0.rightbright/Windows/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace unitrix0.rightbright.Windows
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
