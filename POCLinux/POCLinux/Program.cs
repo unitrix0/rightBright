43a59990d4953f67c7786623472aa16054010d75
mr <mr@unimatrix0.ch>
2025-01-24 15:05:18 +0100

    Test with fixed library

--- POCLinux/POCLinux/Program.cs
+++ POCLinux/POCLinux/Program.cs
@@ -2,7 +2,27 @@
 
 
 using System;
+using System.Text;
 
-var sensor =  YLightSensor.FirstLightSensor();
-
-Console.ReadKey(true);
+var error = "";
+if (YAPI.RegisterHub("usb", ref error) == YAPI.SUCCESS)
+{
+    var sensor =  YLightSensor.FindLightSensor("LIGHTMK3-17AE3E.lightSensor");
+    do
+    {
+        if (sensor.isOnline())
+        {
+            Console.WriteLine($"\r{DateTime.Now.ToShortTimeString()} - {sensor.get_currentValue()}");
+        }
+        else
+        {
+            Console.WriteLine("\rOffline\t\t\t");
+        }
+        
+        Thread.Sleep(1000);
+    } while (!(Console.KeyAvailable&& Console.ReadKey(true).Key == ConsoleKey.Q));
+}
+else
+{
+    Console.WriteLine(error);
+}
