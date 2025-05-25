7c0dd73dd83e5a51b1dfd4bfb34b85d472f91e66
mr <mr@hive.loc>
2025-05-25 19:32:56 +0200

    POC Linux DBus

--- POCLinux/POCLinux/Program.cs
+++ POCLinux/POCLinux/Program.cs
@@ -1,73 +1,50 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
-using System.Timers;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using NetworkManager.DBus;
 using POCLinux.dbus.ddcutil;
+using ScreenBrightness.DBus;
 using Tmds.DBus;
 using Connection = Tmds.DBus.Protocol.Connection;
-using Timer = System.Timers.Timer;
 
 var dbusConnection = new Connection(Address.Session);
 await dbusConnection.ConnectAsync();
 
-var ddcutilSvc = new DdcutilService(dbusConnection, DdcutilService.BusName);
-var ddcutil = ddcutilSvc.CreateDdcutilInterface("/com/ddcutil/DdcutilObject");
+// await WatchAddDisplay(dbusConnection);
+await DdcUtilSetBrightness(dbusConnection);
+async Task WatchAddDisplay(Connection connection)
+{
+    var screenBrightnessSvc = new ScreenBrightnessService(connection, "org.kde.ScreenBrightness");
+    var screenBrightness = screenBrightnessSvc.CreateScreenBrightness("/org/kde/ScreenBrightness");
 
-(int NumberOfDisplays, (int, int, int, string, string, string, ushort, string, uint)[] DetectedDisplays, int ErrorStatus, string ErrorMessage) result = await ddcutil.DetectAsync(0x0);
-var attributes = await ddcutil.GetAttributesReturnedByDetectAsync();
-Console.ReadKey();
-// await ddcutil.SetVcpAsync(2, "", 0x10, 38, 0x0);
-// await ddcutil.SetVcpAsync(2, "", 0x10, 52, 0x0);
+    var names = await screenBrightness.GetDisplaysDBusNamesAsync();
+    foreach (string name in names)
+    {
+        Console.WriteLine(name);
+    }
 
-// Timer handleYapiEventsTimer = new();
-// var error = "";
-// bool init= false;
-//
-// if (YAPI.RegisterHub("usb", ref error) == YAPI.SUCCESS)
-// {
-//     handleYapiEventsTimer.Interval = 2000;
-//     handleYapiEventsTimer.Elapsed += HandleYapiEventsTimerOnElapsed;
-//     
-//     var sensor =  YLightSensor.FindLightSensor("LIGHTMK3-17AE3E.lightSensor");
-//     sensor.clearCache();
-//     sensor.registerTimedReportCallback(TimedReport);
-//     handleYapiEventsTimer.Start();
-//     
-//     do
-//     {
-//         if (sensor.isOnline())
-//         {
-//             
-//             if (!init)
-//             {
-//                 sensor.stopDataLogger();
-//                 sensor.set_logFrequency("OFF");
-//                 init = true;
-//             }
-//             // Console.WriteLine($"\r{DateTime.Now.ToShortTimeString()} - {sensor.get_currentValue()}");
-//         }
-//         else
-//         {
-//             Console.WriteLine("\rOffline\t\t\t");
-//         }
-//         
-//         Thread.Sleep(1000);
-//     } while (!(Console.KeyAvailable&& Console.ReadKey(true).Key == ConsoleKey.Q));
-// }
-// else
-// {
-//     Console.WriteLine(error);
-// }
-//
-// void TimedReport(YLightSensor func, YMeasure measure)
-// {
-//     var currentValue = func.get_currentValue();
-//     Console.WriteLine($"\r{DateTime.Now.ToShortTimeString()} - {currentValue} - timer");
-//     
-// }
-//
-// void HandleYapiEventsTimerOnElapsed(object? sender, ElapsedEventArgs e)
-// {
-//     string errmsg = "";
-//     YAPI.HandleEvents(ref errmsg);
-// }
+    await screenBrightness.WatchDisplayAddedAsync((exception, s) => { Console.WriteLine($"Added: {s}"); });
+    await screenBrightness.WatchDisplayRemovedAsync((exception, s) => { Console.WriteLine($"Removed: {s}"); });
+
+    do
+    {
+        // Console.Write($"\r{DateTime.Now}");
+        // System.Threading.Thread.Sleep(500);
+        await Task.Delay(50);
+    } while (Console.KeyAvailable == false);
+}
+
+async Task DdcUtilSetBrightness(Connection dbusConnection1)
+{
+    var ddcutilSvc = new DdcutilService(dbusConnection1, DdcutilService.BusName);
+    var ddcutil = ddcutilSvc.CreateDdcutilInterface("/com/ddcutil/DdcutilObject");
+
+    var result = await ddcutil.DetectAsync(0x0);
+    var attributes = await ddcutil.GetAttributesReturnedByDetectAsync();
+    var resultSet = await ddcutil.SetVcpAsync(2, "", 0x10, 38, 0x0);
+    // await ddcutil.SetVcpAsync(2, "", 0x10, 52, 0x0);
+    Console.WriteLine("done");
+    
+}
