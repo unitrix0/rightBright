09d3f7f6ec6a96f96a8c78037de7aee030b71905
mr <mr@hive.loc>
2025-09-29 19:41:20 +0200

    Implements custom Icon for Linux

--- rightBright/rightBright/ViewModels/ApplicationViewModel.cs
+++ rightBright/rightBright/ViewModels/ApplicationViewModel.cs
@@ -1,17 +1,18 @@
 ﻿using System;
 using System.ComponentModel;
-using Avalonia;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using Avalonia.Controls;
+using Avalonia.Platform;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using rightBright.Brightness;
-using rightBright.ViewModels;
 
-namespace rightBright.Views;
+namespace rightBright.ViewModels;
 
 public partial class ApplicationViewModel : ViewModelBase
 {
     private readonly IBrightnessController _brightnessController = null!;
-    private readonly MainWindowViewModel _mainWindowViewModel = null!;
 
     public bool Suspend
     {
@@ -20,21 +21,38 @@ public partial class ApplicationViewModel : ViewModelBase
     }
 
     public Action? OnOpenMainWindow;
+    [ObservableProperty] private WindowIcon? _appIcon;
     
     public ApplicationViewModel()
     {
     }
     
-    public ApplicationViewModel(IBrightnessController brightnessController, MainWindowViewModel mainWindowViewModel)
+    public ApplicationViewModel(IBrightnessController brightnessController)
     {
         _brightnessController = brightnessController;
-        _mainWindowViewModel = mainWindowViewModel;
+        SetAppIcon();
     }
-    
+
+    private void SetAppIcon()
+    {
+        var assemblyName= Assembly.GetExecutingAssembly().GetName().Name;
+        var assetUri = RuntimeInformation.IsOSPlatform(OSPlatform.Linux)
+            ? $"avares://{assemblyName}/Assets/AppIcon 16 dark.ico"
+            : $"avares://{assemblyName}/Assets/AppIcon 16 white.ico";
+        
+        AppIcon = new WindowIcon(AssetLoader.Open(new Uri(assetUri)));
+    }
+
     [RelayCommand]
     private void OpenMainWindow()
     {
         OnOpenMainWindow?.Invoke();
+    }
+
+    [RelayCommand]
+    private void ToggleSuspendUpdating()
+    {
+        Suspend = !Suspend;
     }
     
     [RelayCommand]
