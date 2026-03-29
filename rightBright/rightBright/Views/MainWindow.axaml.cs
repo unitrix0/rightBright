d0d72e89f048fca4802daae1e1dc57850aff0696
Marco <marco@unimatrix0.ch>
2026-03-29 11:33:08 +0200

    Adds Version number on titlebar

--- rightBright/rightBright/Views/MainWindow.axaml.cs
+++ rightBright/rightBright/Views/MainWindow.axaml.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using Avalonia.Controls;
 
 namespace rightBright.Views;
@@ -7,5 +8,9 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+
+        var version = Assembly.GetEntryAssembly()?.GetName().Version;
+        if (version is not null)
+            Title = $"rightBright - {version.Major}.{version.Minor}.{version.Build}";
     }
 }
