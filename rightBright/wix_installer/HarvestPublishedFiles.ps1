5f4226d59fa93819460b579549d8626ee5f182db
Marco <marco@unimatrix0.ch>
2026-03-31 15:58:46 +0200

    Fixes the harvest file

--- rightBright/wix_installer/HarvestPublishedFiles.ps1
+++ rightBright/wix_installer/HarvestPublishedFiles.ps1
@@ -15,96 +15,85 @@ if (-not (Test-Path -Path $PublishDir)) {
 Write-Host "Harvesting published files -> $OutFile" -ForegroundColor Cyan
 
 $publishDirFull = (Resolve-Path $PublishDir).Path.TrimEnd('\', '/')
-$allFiles = Get-ChildItem -Path $publishDirFull -Recurse -File
+$allFiles = Get-ChildItem -Path $publishDirFull -Recurse -File |
+    Sort-Object { $_.FullName.Substring($publishDirFull.Length + 1) }
 
-$rootFiles = [System.Collections.Generic.List[System.IO.FileInfo]]::new()
-$subDirFiles = [ordered]@{}
-
-foreach ($f in $allFiles) {
-    $relPath = $f.FullName.Substring($publishDirFull.Length + 1)
-    $relDir  = Split-Path $relPath -Parent
-
-    if ([string]::IsNullOrEmpty($relDir)) {
-        $rootFiles.Add($f)
-    } else {
-        if (-not $subDirFiles.Contains($relDir)) {
-            $subDirFiles[$relDir] = [System.Collections.Generic.List[System.IO.FileInfo]]::new()
-        }
-        $subDirFiles[$relDir].Add($f)
-    }
-}
-
-function Get-WixId([string]$prefix, [string]$path) {
-    $safe = $path -replace '[^a-zA-Z0-9_.]', '_'
-    return "${prefix}_${safe}"
+function Get-SafeId([string]$value) {
+    return ($value -replace '[^a-zA-Z0-9_]', '_')
 }
 
 $sb = [System.Text.StringBuilder]::new()
+$componentIds = [System.Collections.Generic.List[string]]::new()
+$currentDirParts = @()
+
 [void]$sb.AppendLine('<?xml version="1.0" encoding="utf-8"?>')
 [void]$sb.AppendLine('<Wix xmlns="http://wixtoolset.org/schemas/v4/wxs">')
 [void]$sb.AppendLine('  <Fragment>')
-[void]$sb.AppendLine('    <ComponentGroup Id="PublishedComponents" Directory="INSTALLDIR">')
+[void]$sb.AppendLine('    <DirectoryRef Id="INSTALLDIR">')
 
-foreach ($f in $rootFiles) {
-    $name   = $f.Name
-    $idAttr = ""
-    if ($name -ieq "rightBright.exe") { $idAttr = ' Id="RightBrightExe"' }
+foreach ($f in $allFiles) {
+    $relPath  = $f.FullName.Substring($publishDirFull.Length + 1)
+    $relDir   = Split-Path $relPath -Parent
+    $newParts = if ($relDir) { @($relDir -split '\\') } else { @() }
 
-    [void]$sb.AppendLine("      <Component>")
-    [void]$sb.AppendLine("        <File$idAttr Source=`"$name`" />")
-    [void]$sb.AppendLine("      </Component>")
+    # Compute how much of the current directory stack is still valid
+    $commonLen = 0
+    while ($commonLen -lt $currentDirParts.Length -and
+           $commonLen -lt $newParts.Length -and
+           $currentDirParts[$commonLen] -eq $newParts[$commonLen]) {
+        $commonLen++
+    }
+
+    # Close directories we've left
+    for ($i = $currentDirParts.Length - 1; $i -ge $commonLen; $i--) {
+        $indent = '      ' + ('  ' * $i)
+        [void]$sb.AppendLine("$indent</Directory>")
+    }
+
+    # Open directories we've entered
+    for ($i = $commonLen; $i -lt $newParts.Length; $i++) {
+        $dirPath = ($newParts[0..$i] -join '\')
+        $dirId   = "dir_$(Get-SafeId $dirPath)"
+        $indent  = '      ' + ('  ' * $i)
+        [void]$sb.AppendLine("$indent<Directory Id=`"$dirId`" Name=`"$($newParts[$i])`">")
+    }
+
+    $currentDirParts = $newParts
+
+    # Write the Component + File element
+    $cmpId  = "cmp_$(Get-SafeId $relPath)"
+    $depth  = $newParts.Length
+    $indent = '      ' + ('  ' * $depth)
+
+    $fileIdAttr = ''
+    if ($f.Name -ieq 'rightBright.exe') { $fileIdAttr = ' Id="RightBrightExe"' }
+
+    [void]$sb.AppendLine("$indent<Component Id=`"$cmpId`">")
+    [void]$sb.AppendLine("$indent  <File$fileIdAttr Source=`"$relPath`" />")
+    [void]$sb.AppendLine("$indent</Component>")
+
+    $componentIds.Add($cmpId)
 }
 
-foreach ($dir in $subDirFiles.Keys) {
-    $dirId = Get-WixId "dir" $dir
-    foreach ($f in $subDirFiles[$dir]) {
-        $relPath = $f.FullName.Substring($publishDirFull.Length + 1)
-        [void]$sb.AppendLine("      <Component Directory=`"$dirId`">")
-        [void]$sb.AppendLine("        <File Source=`"$relPath`" />")
-        [void]$sb.AppendLine("      </Component>")
-    }
+# Close any directories still open
+for ($i = $currentDirParts.Length - 1; $i -ge 0; $i--) {
+    $indent = '      ' + ('  ' * $i)
+    [void]$sb.AppendLine("$indent</Directory>")
+}
+
+[void]$sb.AppendLine('    </DirectoryRef>')
+[void]$sb.AppendLine('  </Fragment>')
+[void]$sb.AppendLine('')
+[void]$sb.AppendLine('  <Fragment>')
+[void]$sb.AppendLine('    <ComponentGroup Id="PublishedComponents">')
+
+foreach ($id in $componentIds) {
+    [void]$sb.AppendLine("      <ComponentRef Id=`"$id`" />")
 }
 
 [void]$sb.AppendLine('    </ComponentGroup>')
-
-$sortedDirs = $subDirFiles.Keys | Sort-Object
-if ($sortedDirs) {
-    $allDirPaths = [ordered]@{}
-    foreach ($dir in $sortedDirs) {
-        $parts = $dir -split '\\'
-        for ($i = 0; $i -lt $parts.Length; $i++) {
-            $current = ($parts[0..$i] -join '\')
-            if (-not $allDirPaths.Contains($current)) {
-                $parent = if ($i -gt 0) { ($parts[0..($i - 1)] -join '\') } else { $null }
-                $allDirPaths[$current] = @{
-                    Name   = $parts[$i]
-                    Parent = $parent
-                    Id     = Get-WixId "dir" $current
-                }
-            }
-        }
-    }
-
-    function Write-DirectoryTree([string]$parentPath, [string]$indent) {
-        $children = $allDirPaths.GetEnumerator() |
-            Where-Object { $_.Value.Parent -eq $parentPath } |
-            Sort-Object Key
-        foreach ($child in $children) {
-            $info = $child.Value
-            [void]$sb.AppendLine("$indent<Directory Id=`"$($info.Id)`" Name=`"$($info.Name)`">")
-            Write-DirectoryTree $child.Key "$indent  "
-            [void]$sb.AppendLine("$indent</Directory>")
-        }
-    }
-
-    [void]$sb.AppendLine('')
-    [void]$sb.AppendLine('    <DirectoryRef Id="INSTALLDIR">')
-    Write-DirectoryTree $null "      "
-    [void]$sb.AppendLine('    </DirectoryRef>')
-}
-
 [void]$sb.AppendLine('  </Fragment>')
 [void]$sb.AppendLine('</Wix>')
 
 $sb.ToString() | Set-Content -Path $OutFile -Encoding UTF8
-Write-Host "Generated $($allFiles.Count) file entries" -ForegroundColor Green
+Write-Host "Generated $($allFiles.Count) file entries in $($componentIds.Count) components" -ForegroundColor Green
